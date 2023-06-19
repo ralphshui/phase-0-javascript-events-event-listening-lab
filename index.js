0766756412ba423685933285e7a446e2fd8f058e
Ralph Shui <rskc.2112@gmail.com>
2023-06-19 06:05:22 -0400

    completing lab

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 function addingEventListener() {
     const input = document.getElementById('button');
     alert('I was clicked!');
-   
     input.addEventListener('click', addingEventListener);
-
 }
 
-addingEventListener();
